0de175ad047b8decd26b16b14e8f78eb27519003
efra <eliassaldarriaga65@outlook.es>
2023-02-03 16:23:40 -0500

    agregando otra linea print

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         print("Esto es un ejemplo")
+        print("agregando mas lineas")
     }
 }
