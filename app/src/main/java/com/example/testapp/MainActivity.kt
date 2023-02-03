d309faf8aa326b37187809e7b7148d62458caaf1
efra <eliassaldarriaga65@outlook.es>
2023-02-03 16:40:40 -0500

    comment face

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         print("Esto es un ejemplo")
+        // comment :')
 
     }
 }
